928b3aa78f2bb85335152b61c29192fe19a54f44
jimin-mjl <jiminaleejung@gmail.com>
2022-07-05 16:10:21 +0900

    feat: 반복문 연습 코드 추가

---
+++ study/study.cpp
@@ -0,0 +1,82 @@
+﻿// study.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    #pragma region 별만들기 게임 
+    // 사용자에게서 정수 N을 받아서 N*N 모양의 별을 만들어라
+
+    int input;
+    cin >> input;
+
+    for (int i = 0; i < input; i++) {
+        for (int k = 0; k < input; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    //cout << "booya! \n";
+    #pragma endregion
+
+    #pragma region 별만들기 게임2 
+    // 사용자에게서 정수 N을 받아서 별이 N줄까지 하나부터 순차적으로 증가하도록 만들어라
+
+    int input2;
+    cin >> input2;
+
+    for (int i = 0; i < input2; i++) {
+        for (int k = 0; k < i+1; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    cout << "Booya! \n";
+    #pragma endregion
+
+    #pragma region 별만들기 게임3
+    // 사용자에게서 정수 N을 받아서 별이 N줄까지 N개부터 순차적으로 줄어들도록 만들어라
+
+    int input3;
+    cin >> input3;
+
+    for (int i = 0; i < input3; i++) {
+        for (int k = input3; k > i; k--) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    cout << "Booya! \n";
+    #pragma endregion
+
+    #pragma region 구구단
+    // 2단부터 9단까지의 구구단을 출력하시오
+    cout << "구구단을 출력합니다... \n";
+    cout << endl;
+
+    for (int i = 2; i < 10; i++) {
+        for (int k = 2; k < 10; k++) {
+            cout << i << " x " << k << " = " << i * k << endl;
+        }
+    }
+
+    cout << endl;
+    cout << "구구단을 출력이 완료되었습니다! \n";
+    #pragma endregion
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
